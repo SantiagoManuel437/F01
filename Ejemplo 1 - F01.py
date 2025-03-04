407f9852931457781ebd475e32c113303095db80
SantiagoManuel437 <estradajohan51@gmail.com>
2025-03-03 23:22:41 -0600

    Ejemplo 1 - F01.py

---
+++ Ejemplo 1 - F01.py
@@ -0,0 +1,19 @@
+# Losrafikis
+# Este codigo define seis variables de las cuales dos son ocupadas como datos y cuatro como resultados de operaciones simples para luego ser impresas por medio de print en la consola
+# 2025/03/03 - V.1.0 se agrega encabezado y comentarios al codigo 
+# trabajo: maxvazquez56 y carlosstrk
+# phyton no requiere la definicion del tipoi de dato que se ingresa
+numero1 = 5 # el numero 5 es el valor con el que se define el numero1
+numero2 = 3 # el numero 3 es el valor con el que se define el numero 2
+
+
+suma = numero1 + numero2 # se define la operacion de suma con el valor de la suma del numero1 y numero2
+resta = numero1 - numero2 # se define la operacion de resta con el valor de la resta del numero1 y numero2
+multiplicacion = numero1 * numero2 # se define la operacion de multiplicacion con el valor de la multiplicacion del numero1 y numero2
+division = numero1 / numero2 # se define la operacion de division con el valor de la division del numero1 y numero2
+
+
+print("Suma:", suma) #se imprime en la consola la palabra "Suma" acompañado de ":" seguido se imprime el valor de suma
+print("Resta:", resta) #se imprime en la consola la palabra "resta" acompañado de ":" seguido se imprime el valor de resta
+print("Multiplicación:", multiplicacion) #se imprime en la consola la palabra "multiplicacion" acompañado de ":" seguido se imprime el valor de multiplicacion
+print("División:", division) #se imprime en la consola la palabra "division" acompañado de ":" seguido se imprime el valor de division
